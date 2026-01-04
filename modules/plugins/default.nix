f1458d39401a653ea325ae42469eedd879c09c66
Jacek Sawiński <jacek.sawinski.0905@gmail.com>
2026-01-04 22:22:06 +0100

    Removed util

--- modules/plugins/default.nix
+++ modules/plugins/default.nix
@@ -1,11 +1,11 @@
-{...}:
+{ lib, ...}:
 
 let
   file_to_not_import = [
     "default.nix"
   ];
 
-  get-import-dir = dir: ignore: import ./util/get-import-dir.nix {lib = nixpkgs.lib; inherit dir; inherit ignore;};
+  get-import-dir = dir: ignore: import ../../util/get-import-dir.nix {inherit lib; inherit dir; inherit ignore;};
 in
 {
   imports = get-import-dir ./. file_to_not_import;
