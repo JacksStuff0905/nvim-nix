cb9b9b3337aa33a9771a5e759ea91e7ec981c566
Jacek Sawiński <jacek.sawinski.0905@gmail.com>
2026-01-12 19:26:33 +0100

    Added java & json

---
+++ modules/languages/json.nix
@@ -0,0 +1,26 @@
+{config, lib, pkgs, ...}:
+
+let
+  name = "json";
+  lsp-name = "json";
+
+  cfg = config.programs.nvim-nix.languages;
+in
+{
+  options.programs.nvim-nix.languages."${name}" = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      description = "Enable ${name} language module";
+      default = true;
+    };
+  };
+
+  config.vim = lib.mkIf (cfg.enable && cfg."${name}".enable) {
+    languages."${lsp-name}" = {
+      enable = true;
+      treesitter.enable = cfg.treesitter;
+      lsp.enable = cfg.lsp;
+      format.enable = cfg.format;
+    };
+  };
+}
