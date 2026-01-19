fed4eba7ac62ee3feb93caf02fff2b8cee76659e
Jacek Sawiński <jacek.sawinski.0905@gmail.com>
2026-01-19 23:34:03 +0100

    Formatting

--- modules/opts/undo.nix
+++ modules/opts/undo.nix
@@ -1,27 +1,27 @@
-{config, lib, ...}:
+{ config, lib, ... }:
 
 let
-        cfg = config.programs.nvim-nix.opts.undo;
+  cfg = config.programs.nvim-nix.opts.undo;
 in
 {
 
-        options.programs.nvim-nix.opts.undo = {
-                enable = lib.mkOption {
-                        type = lib.types.bool;
-                        default = true;
-                };
+  options.programs.nvim-nix.opts.undo = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+    };
 
-                persistent = {
-                        enable = lib.mkOption {
-                                type = lib.types.bool;
-                                default = true;
-                        };
-                };
-        };
+    persistent = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+      };
+    };
+  };
 
-        config.vim = lib.mkIf config.programs.nvim-nix.opts.enable {
-                undoFile = lib.mkIf (cfg.enable && cfg.persistent.enable) {
-                        enable = true;
-                };
-        };
+  config.vim = lib.mkIf config.programs.nvim-nix.opts.enable {
+    undoFile = lib.mkIf (cfg.enable && cfg.persistent.enable) {
+      enable = true;
+    };
+  };
 }
