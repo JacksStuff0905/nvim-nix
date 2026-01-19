951e4f916bd1339dbb92bf4b0e524c65b9b782f5
Jacek Sawiński <jacek.sawinski.0905@gmail.com>
2026-01-19 18:48:47 +0100

    Added clipboard providers as dependencies

--- modules/opts/clipboard.nix
+++ modules/opts/clipboard.nix
@@ -1,35 +1,42 @@
-{config, lib, ...}:
+{ config, lib, pkgs, ... }:
 
 let
-        cfg = config.programs.nvim-nix.opts.clipboard;
+  cfg = config.programs.nvim-nix.opts.clipboard;
 in
 {
 
-        options.programs.nvim-nix.opts.clipboard = {
-                enable = lib.mkOption {
-                        type = lib.types.bool;
-                        default = true;
-                };
+  options.programs.nvim-nix.opts.clipboard = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+    };
 
-                registers = lib.mkOption {
-                        type = lib.types.str;
-                        default = "unnamedplus";
-                };
+    registers = lib.mkOption {
+      type = lib.types.str;
+      default = "unnamedplus";
+    };
 
-                providers = {
-                        wl-copy.enable = lib.mkOption {
-                                type = lib.types.bool;
-                                default = true;
-                        };
+    providers = {
+      wl-copy.enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+      };
 
-                        xclip.enable = lib.mkOption {
-                                type = lib.types.bool;
-                                default = true;
-                        };
-                };
-        };
+      xclip.enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+      };
+    };
+  };
 
-        config.vim = lib.mkIf config.programs.nvim-nix.opts.enable {
-                clipboard = lib.mkIf config.programs.nvim-nix.opts.clipboard.enable cfg;
-        };
+  config.vim = lib.mkIf (config.programs.nvim-nix.opts.enable && cfg.enable) {
+
+    # Extra dependencies
+    extraPackages = with pkgs; lib.mkMerge [
+      [(lib.mkIf cfg.providers.wl-copy.enable wl-clipboard)]
+      [(lib.mkIf cfg.providers.xclip.enable xclip)]
+    ];
+
+    clipboard = cfg;
+  };
 }
