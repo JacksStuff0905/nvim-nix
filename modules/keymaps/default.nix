aa1c2961fff64d5986fd09f2ba8060990292d91a
Jacek Sawiński <jacek.sawinski.0905@gmail.com>
2026-01-29 18:53:40 +0100

    Configured godot lsp

--- modules/keymaps/default.nix
+++ modules/keymaps/default.nix
@@ -1,49 +1,105 @@
-{config, lib, ...}:
+{ config, lib, ... }:
 let
-        cfg = config.programs.nvim-nix.global-keymaps;
+  cfg = config.programs.nvim-nix.global-keymaps;
 in
 {
-        # Configure global keymaps here.
-        # If a keymap is used for a plugin, it should be placed in the plugin's config instead.
+  # Configure global keymaps here.
+  # If a keymap is used for a plugin, it should be placed in the plugin's config instead.
 
-        options.programs.nvim-nix.global-keymaps = {
-                enable = lib.mkEnableOption "Enable global keymaps module";
+  options.programs.nvim-nix.global-keymaps = {
+    enable = lib.mkEnableOption "Enable global keymaps module";
 
-                window-navigation = {
-                        enable = lib.mkOption { type = lib.types.bool; default = true; };
+    window-navigation = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+      };
 
-                        left = lib.mkOption { type = lib.types.str; default = "H"; };
-                        right = lib.mkOption { type = lib.types.str; default = "L"; };
-                        down = lib.mkOption { type = lib.types.str; default = "J"; };
-                        up = lib.mkOption { type = lib.types.str; default = "K"; };
+      left = lib.mkOption {
+        type = lib.types.str;
+        default = "H";
+      };
+      right = lib.mkOption {
+        type = lib.types.str;
+        default = "L";
+      };
+      down = lib.mkOption {
+        type = lib.types.str;
+        default = "J";
+      };
+      up = lib.mkOption {
+        type = lib.types.str;
+        default = "K";
+      };
 
-                        previous = lib.mkOption { type = lib.types.str; default = "<leader>bk"; };
-                        next = lib.mkOption { type = lib.types.str; default = "<leader>fw"; };
-                };
+      previous = lib.mkOption {
+        type = lib.types.str;
+        default = "<leader>bk";
+      };
+      next = lib.mkOption {
+        type = lib.types.str;
+        default = "<leader>fw";
+      };
+    };
 
-                search-highlighting = {
-                        enable = lib.mkOption { type = lib.types.bool; default = true; };
+    search-highlighting = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+      };
 
-                        key = lib.mkOption { type = lib.types.str; default = "<leader>nh"; };
-                };
-        };
+      key = lib.mkOption {
+        type = lib.types.str;
+        default = "<leader>nh";
+      };
+    };
+  };
 
+  config.vim.keymaps = lib.mkIf cfg.enable (
+    lib.mkMerge [
+      # Configure window navigation
+      (lib.mkIf cfg.window-navigation.enable [
+        {
+          key = cfg.window-navigation.left;
+          mode = "n";
+          action = "<C-w>h";
+        }
+        {
+          key = cfg.window-navigation.down;
+          mode = "n";
+          action = "<C-w>j";
+        }
+        {
+          key = cfg.window-navigation.up;
+          mode = "n";
+          action = "<C-w>k";
+        }
+        {
+          key = cfg.window-navigation.right;
+          mode = "n";
+          action = "<C-w>l";
+        }
 
-        config.vim.keymaps = lib.mkIf cfg.enable (lib.mkMerge [
-                # Configure window navigation
-                (lib.mkIf cfg.window-navigation.enable [
-                        { key = cfg.window-navigation.left; mode = "n"; action = "<C-w>h"; }
-                        { key = cfg.window-navigation.down; mode = "n"; action = "<C-w>j"; }
-                        { key = cfg.window-navigation.up; mode = "n"; action = "<C-w>k"; }
-                        { key = cfg.window-navigation.right; mode = "n"; action = "<C-w>l"; }
+        {
+          key = cfg.window-navigation.previous;
+          mode = "n";
+          action = "<C-o>";
+        }
+        {
+          key = cfg.window-navigation.next;
+          mode = "n";
+          action = "<C-i>";
+        }
+      ])
 
-                        { key = cfg.window-navigation.previous; mode = "n"; action = "<C-o>"; }
-                        { key = cfg.window-navigation.next; mode = "n"; action = "<C-i>"; }
-                ])
-
-                # Configure seach highlighting
-                (lib.mkIf cfg.search-highlighting.enable [
-                        { key = cfg.search-highlighting.key; mode = "n"; action = ":noh<CR>"; }
-                ])
-        ]);
+      # Configure seach highlighting
+      (lib.mkIf cfg.search-highlighting.enable [
+        {
+          key = cfg.search-highlighting.key;
+          mode = "n";
+          action = ":noh<CR>";
+        }
+      ])
+    ]
+  );
 }
--- modules/languages/gdscript.nix
+++ modules/languages/gdscript.nix
@@ -1,8 +1,13 @@
-{config, lib, pkgs, ...}:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   name = "gdscript";
-  lsp-name = "godot";
+  lsp-name = "gdscript";
 
   cfg = config.programs.nvim-nix.languages;
 in
@@ -19,27 +24,32 @@ in
     treesitter = lib.mkIf cfg.treesitter {
       enable = true;
       highlight.enable = true;
-      
+
       grammars = [ pkgs.vimPlugins.nvim-treesitter-parsers."${name}" ];
     };
 
-    lsp.lspconfig.sources."${name}" = ''
-      require("lspconfig")."${name}".setup({
-        name = "${lsp-name}",
-        cmd = vim.lsp.rpc.connect("127.0.0.1", 6005),
-        
-        -- When client attaches
-        on_init = function(client, init_result)
-          local pipe = "/tmp/godot.pipe"
-          
-          local success, err = pcall(vim.fn.serverstart, pipe)
-          if not success then
-            print("Note: Failed to start Godot pipe (might already exist): " .. err)
-          else
-            print("Godot LSP connected. Pipe at: " .. pipe)
-          end
-        end,
-      })
-    '';
+    lsp.lspconfig = {
+      enable = true;
+      sources."${lsp-name}" = ''
+        vim.lsp.config['${lsp-name}'] = ({
+          name = "${lsp-name}",
+          cmd = vim.lsp.rpc.connect("127.0.0.1", 6005),
+
+          -- When client attaches
+          on_init = function(client, init_result)
+            local pipe = "/tmp/godot.pipe"
+            
+            local success, err = pcall(vim.fn.serverstart, pipe)
+            if not success then
+              print("Note: Failed to start Godot pipe (might already exist): " .. err)
+            else
+              print("Godot LSP connected. Pipe at: " .. pipe)
+            end
+          end,
+        })
+
+        vim.lsp.enable('${lsp-name}')
+      '';
+    };
   };
 }
