7ccf0c5c513bd19343af8d2527b5b9082509927b
Jacek Sawiński <jacek.sawinski.0905@gmail.com>
2026-01-29 20:08:19 +0100

    Lsp config update

---
+++ modules/languages/lua.nix
@@ -0,0 +1,27 @@
+
+{config, lib, pkgs, ...}:
+
+let
+  name = "lua";
+  lsp-name = "lua";
+
+  cfg = config.programs.nvim-nix.languages;
+in
+{
+  options.programs.nvim-nix.languages."${name}" = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      description = "Enable ${name} language module";
+      default = true;
+    };
+  };
+
+  config.vim = lib.mkIf (cfg.enable && cfg."${name}".enable) {
+    languages."${lsp-name}" = {
+      enable = true;
+      treesitter.enable = cfg.treesitter;
+      lsp.enable = cfg.lsp;
+      format.enable = cfg.format;
+    };
+  };
+}
